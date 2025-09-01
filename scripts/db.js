8edf20625f8feefd3d514895fbfa0d648517c06a
Jermaine Jérôme <jay.jero1901@gmail.com>
2025-09-01 20:51:12 +0200

    Created full dishes array in db.js

---
+++ scripts/db.js
@@ -0,0 +1,82 @@
+let dishes = [
+    {
+        "name": "Classic Cheeseburger",
+        "price": 8.99,
+        "description": "Saftiger Rindfleisch-Patty mit Cheddar, frischem Salat, Tomaten, Gurken und hausgemachter Sauce.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Classic Beef"
+    },
+    {
+        "name": "Double Cheese Burger",
+        "price": 11.49,
+        "description": "Zwei Rindfleisch-Patties, doppelt Cheddar, frische Tomaten, Gurken und unsere Spezialsoße.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Classic Beef"
+    },
+    {
+        "name": "Chili Cheese Burger",
+        "price": 10.99,
+        "description": "Saftiger Beef-Patty mit Jalapeños, geschmolzenem Käse und feuriger Chili-Sauce.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Special Beef"
+    },
+    {
+        "name": "BBQ Bacon Burger",
+        "price": 10.49,
+        "description": "Rinder-Patty mit knusprigen Bacon, rauchiger BBQ Sauce, Zwiebelringen und Cheddar.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Special Beef"
+    },
+    {
+        "name": "Pulled Pork Burger",
+        "price": 12.49,
+        "description": "Zart geschmortes Pulled Pork mit Cole Slaw und BBQ-Sauce im Brioche-Bun.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Special Beef"
+    },
+    {
+        "name": "Chicken Crispy Burger",
+        "price": 9.99,
+        "description": "Knuspriges Hähnchenfilet im Bun mit Salat, Tomaten und Honig-Senf-Soße.", 
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Chicken"
+    },
+    {
+        "name": "Veggie Deluxe Burger",
+        "price": 9.49,
+        "description": "Hausgemachtes Gemüse-Patty mit Avocado-Creme, frischem Salat und knusprigem Ciabatta-Bun.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Veggie"
+    },
+    {
+        "name": "Avocado Veggie Burger",
+        "price": 9.99,
+        "description": "Vegetarischer Patty mit Avocado, frischen Sprossen, Tomaten und hausgemachter Kräutersoße.",
+        "amount": 0,
+        "category": "Burger",
+        "subCategory": "Veggie"
+    },
+    {
+        "name": "Pizza Margherita",
+        "price": 7.99,
+        "description": "Knuspriger Teig mit fruchtiger Tomatensauce, frischem Mozzarella und Basilikium.",
+        "amount": 0,
+        "category": "Pizza",
+        "subCategory": "Classic"
+    },
+    {
+        "name": "Pizza Diavolo",
+        "price": 9.99,
+        "description": "Feurig-scharf mit Salami, Peperoni, Chili und extra viel Käse.",
+        "amount": 0,
+        "category": "Pizza",
+        "subCategory": "Spicy"
+    }
+];
